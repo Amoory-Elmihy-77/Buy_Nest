92b5313be057ff353bd91b79ede222027c135f26
Ammar Elmihy <amaryasser2358@gmail.com>
2025-07-26 22:54:50 +0300

    set up the env and folder structure and main layout

---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import { MainLayout } from "@layouts/index";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+createRoot(document.getElementById("root")!).render(<MainLayout />);
